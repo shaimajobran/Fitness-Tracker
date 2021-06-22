b5414b4e317f29b5d6d9aeb6db700cb36bf5b267
Shaima Jobran <shaimajobran22@gmail.com>
2021-06-22 17:33:45 -0400

    server file

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const db = require("./models");
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+//routes 
+app.use(require("./routes/htmlRoutes"));
+app.use(require("./routes/apiRoutes"));       // this time we will have the routes files so we dont need to write any Get,Post,Est.. here
+
+//mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populate", { useNewUrlParser: true });
+
+//connecting to dtatbase
+//creating new instance of mongoDB using mongojs library
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
